3ab3f231e34ac8494ed302449364314eff4c7f87
Ahnaf Hasan <ahasan1@stuy.edu>
2018-09-29 16:35:39 -0400

    add header file

---
+++ str.h
@@ -0,0 +1,8 @@
+int myStrlen(char * len);
+char * myStrcpy(char * dest, char * source);
+char * myStrncpy(char * dest, char * source, int limit);
+int myStrcmp(char *s1, char *s2);
+char * myStrchar(char * location, char wanted);
+char * myStrcat(char * dest, char * source);
+char * myStrncat( char *dest, char *source ,int n);
+char * myStrstr(char *s1, char *s2 );
