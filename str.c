cb9f9424408b9df2f6df27d2c6d3f0e27956da86
Ahnaf Hasan <ahasan1@stuy.edu>
2018-09-28 21:26:13 -0400

    add strcmp && fix strcpy

--- str.c
+++ str.c
@@ -10,32 +10,25 @@ int myStrlen(char * len) {
 }
 
 char * myStrcpy(char * dest, char * source) {
-  char retChar[256];
-  int i =0;
-  for (; ; *dest++) {
-    retChar[i] = *dest;
-    i++;
+  while(*dest) {
+    *dest++ = *source++;
   }
-  for (; ;*source++) {
-    retChar[i] = *source;
-    i++;
-  }
-  
-  return retChar;
+  return dest;
 }
 
 int myStrcmp(char *s1, char *s2) {
-  char * first = s1;
-  char * second = s2;
-  while(*first++) {
-    if (*second++ == '\0') {
+  while( 1 ) {
+    if (*s1 == '\0' && *s2 == '\0') {
+      return 0;
+    }
+    if (*s1 == '\0') {
+      return -99;
+    }
+    if (*s2 == '\0') {
       return 99;
     }
-  }
-  if (*second++ != '\0') {
-    return -99;
-  }else{
-    return 0;
+    s1++;
+    s2++;
   }
 }
 
@@ -54,11 +47,10 @@ int main() {
   char len[] = "Hello";
   char str[] = "Hello";
   char ert[] = "Person";
-  printf("length is %i\n", myStrlen(len));
-  // printf("cpy %p, %s\n", myStrcpy(len, str), len);
-  printf("Compare: %s to %s,  %i\n        %s to %s, %i\n",
-	 len, str, myStrcmp(len, str), str, ert, myStrcmp(str, ert));
-   printf("Str char: where does %s have %c?\n%c is at: %p\n actual location: %p\n",
+  printf("length is %i\n", myStrlen(len) );
+  printf("Compare: %s to %s,  %i\n", len, str, myStrcmp(len, str));
+  printf("cpy:at %p, len is now %s\n", myStrcpy(len, ert), len );
+  printf("Str char: where does %s have %c?\n%c is at: %p\n actual location: %p\n",
           ert, 's', 's', myStrchar(ert, 's'), strchr(ert, 's'));
   
   return 0;
